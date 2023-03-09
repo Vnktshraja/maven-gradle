851c1b13ff6a1194b70489aec7b04c2adda0dcce
venkateshraja <vnktshraja@gmail.com>
2023-03-09 10:14:18 +0530

    first commit

---
+++ src/main/java/com/code/mavengradle/MavenGradleApplication.java
@@ -0,0 +1,13 @@
+package com.code.mavengradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenGradleApplication.class, args);
+	}
+
+}
